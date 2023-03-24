3f1110dcfc58737da9bc2b16168dc37b5027aace
thuyhang <thuyhangfr01@gmail.com>
2023-03-25 01:29:29 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css"
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+    <Header/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Homee</div>
+  )
+}
+
+export default Home
