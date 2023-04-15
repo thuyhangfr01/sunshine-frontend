6c5dad2aca3922e47b9863b3696193ba4a6261fd
thuyhang <thuyhangfr01@gmail.com>
2023-04-15 07:41:22 +0700

    add component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css"
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
     <Header/>
       <Routes>
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Home />} /> 
       </Routes>
     <Footer />
     </>
